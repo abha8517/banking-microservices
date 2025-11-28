8571e1674a3c6ddf2f78081d0d98bcb38120628b
abhdubey7 <developerforjava789@gmail.com>
2025-11-28 20:30:51 +0530

    Fixing the Security Config for Grpc

---
+++ auth-service/src/main/java/com/company/banking/authservice/config/GrpcSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.company.banking.authservice.config;
+
+import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcSecurityConfig {
+    @Bean
+    public GrpcAuthenticationReader grpcAuthenticationReader() {
+        // CHANGE: Use BasicGrpcAuthenticationReader or customize as needed
+        return new BasicGrpcAuthenticationReader();
+    }
+}
